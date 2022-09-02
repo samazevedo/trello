5849e345d92f2c814aa2eb479b2c98a004db3357
samzevedo <samuel.azevedo@live.com>
2022-09-02 02:19:40 -0300

    add onkeydown event function to new item form

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -9,12 +9,20 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState('')
     const inputRef = useFocus()
+
+    const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            onAdd(text)
+        }
+    }
+
     return (
         <NewItemFormContainer>
             <NewItemInput
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 ref={inputRef}
+                onKeyDown={handleAddText}
             />
             <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
         </NewItemFormContainer>
